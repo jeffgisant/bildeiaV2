539f8f239fbd44bc99095b34b2b64e5405018288
jeffgisant <99454554+jeffgisant@users.noreply.github.com>
2023-02-14 00:07:04 -0600

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'ef056ab9cd8b466ba26c778608c6b078',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -11,7 +11,7 @@ export default siteConfig({
   // basic site info (required)
   name: 'Blog Bildeia',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Bildeia',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
